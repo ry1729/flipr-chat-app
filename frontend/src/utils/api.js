c5e07cabf450d18892667f71f6c6b2317e543252
Balabathuni Pranathi <167720864+pranathibalabathuni@users.noreply.github.com>
2025-05-26 09:45:19 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL || 'https://flipr-chat-app-be.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
